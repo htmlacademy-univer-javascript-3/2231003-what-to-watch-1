877efec9baea7a316f4ef1d81773226f99e2f470
Lycoris-nya <cat26022002@mail.ru>
2023-01-09 18:38:29 +0500

    more tests

---
+++ project/src/pages/main-screen/main-screen.test.tsx
@@ -0,0 +1,45 @@
+import {Provider} from 'react-redux';
+import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import HistoryRouter from '../../components/history/history';
+import {AuthorizationStatus} from '../../const';
+import MyList from './list-screen';
+import {makeFakeFilm} from '../../utils/mocks';
+
+const mockStore = configureMockStore();
+const history = createMemoryHistory();
+const mockFilms = [makeFakeFilm()];
+
+
+const store = mockStore({
+  AUTH_INFO: {
+    authorizationStatus: AuthorizationStatus.Auth
+  },
+  FAVORITE_DATA: {
+    favoriteFilms: mockFilms,
+    favoritesCount: mockFilms.length,
+    areFavoriteLoading: false,
+    areFavoriteOutdated: false,
+  }
+});
+
+jest.mock('react', () => ({
+  ...jest.requireActual("react"),
+  useEffect: jest.fn()
+}))
+
+describe('Component: MyList', () => {
+  it('should render correctly', () => {
+    render(
+      <Provider store={store}>
+      <HistoryRouter history={history}>
+        <MyList/>
+        </HistoryRouter>
+        </Provider>,
+    );
+
+    expect(screen.getByText('My list')).toBeInTheDocument();
+    expect(screen.getByText(/Catalog/i)).toBeInTheDocument();
+  });
+});
---
+++ project/src/pages/movie-page-screen/movie-page-screen.test.tsx
@@ -0,0 +1,61 @@
+import {Provider} from 'react-redux';
+import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import HistoryRouter from '../../components/history/history';
+import {AuthorizationStatus} from '../../const';
+import MoviePageScreen from './movie-page-screen';
+import {makeFakeFilm} from '../../utils/mocks';
+
+const mockStore = configureMockStore();
+const history = createMemoryHistory();
+const mockFilms = [makeFakeFilm()];
+const mockFilm = makeFakeFilm();
+
+
+const store = mockStore({
+  AUTH_INFO: {
+    authorizationStatus: AuthorizationStatus.Auth
+  },
+  GENERAL_DATA: {
+    allFilms: mockFilms,
+    genresList: ['All genres'],
+    genreToFilms: {},
+    promo: mockFilm,
+    currentGenre: 'All genres',
+    pageFilms: [],
+    page: 1,
+    isLastPage: false,
+    allFilmsLoading: false,
+    promoLoading: false,
+  },
+  FILM_REVIEWS_DATA: {
+    reviewsFilmId: 0,
+    reviewsLoading: false,
+    reviews: []
+  },
+  FAVORITE_DATA: {
+    favoriteFilms: [],
+    favoriteLoading: false,
+  }
+});
+
+jest.mock('react', () => ({
+  ...jest.requireActual("react"),
+  useEffect: jest.fn()
+}))
+
+describe('Component: Main', () => {
+  it('should render correctly', () => {
+    render(
+      <Provider store={store}>
+      <HistoryRouter history={history}>
+        <MainScreen/>
+        </HistoryRouter>
+        </Provider>,
+    );
+
+    expect(screen.getByText('Play')).toBeInTheDocument();
+    expect(screen.getByText(/Catalog/i)).toBeInTheDocument();
+  });
+});
